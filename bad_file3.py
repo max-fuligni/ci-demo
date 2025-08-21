dec22dfc92b9138a9844dff94c3ccbe2e235fab1
max-fuligni <217225714+max-fuligni@users.noreply.github.com>
2025-08-21 13:00:24 +0000

    ci: apply Ruff/Black auto-fixes

--- bad_file3.py
+++ bad_file3.py
@@ -1,27 +1,31 @@
+print("starting up")
+x = 1 + 2
+from typing import *
 
-print('starting up');x=  1+2   
-import os, sys  
-from math import sqrt as s 
-import math     
-from typing import *  
-
-list = 0  
+list = 0
 
 VERY_LONG = "this is a very very very very very very very very very very very very very very very very very very very long line that black will wrap and reformat because it exceeds the typical 88 character limit used by black making --check fail on purpose"
 
-def bad(a,b,c): return a+b+c  
 
-def do_stuff(l=[1,2,3], data={'a':1}):  
-    unused = 123  
-    if l==None:  
-        l=[]
+def bad(a, b, c):
+    return a + b + c
+
+
+def do_stuff(l=[1, 2, 3], data={"a": 1}):
+    unused = 123
+    if l == None:
+        l = []
     try:
-        for i in range(0,10): y=i  
-    except:  
+        for i in range(0, 10):
+            y = i
+    except:
         pass
-    return sum([n*2  for n in l])  
+    return sum([n * 2 for n in l])
 
-class Foo: pass  
 
-if __name__=="__main__":  
-    print ( "done" )  
+class Foo:
+    pass
+
+
+if __name__ == "__main__":
+    print("done")
